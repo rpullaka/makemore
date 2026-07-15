653b04378dfc335557b3c74944a6746761e902d4
Rpullaka <rpullaka@gmail.com>
2026-07-14 21:21:13 -0700

    Code changes

--- makemore.py
+++ makemore.py
@@ -601,7 +601,7 @@ if __name__ == '__main__':
     parser.add_argument('--resume', action='store_true', help="when this flag is used, we will resume optimization from existing model in the workdir")
     parser.add_argument('--sample-only', action='store_true', help="just sample from the model and quit, don't train")
     parser.add_argument('--num-workers', '-n', type=int, default=4, help="number of data workers for both train/test")
-    parser.add_argument('--max-steps', type=int, default=-1, help="max number of optimization steps to run for, or -1 for infinite.")
+    parser.add_argument('--max-steps', type=int, default=1000, help="max number of optimization steps to run for, or -1 for infinite.")
     parser.add_argument('--device', type=str, default='cpu', help="device to use for compute, examples: cpu|cuda|cuda:2|mps")
     parser.add_argument('--seed', type=int, default=3407, help="seed")
     # sampling
